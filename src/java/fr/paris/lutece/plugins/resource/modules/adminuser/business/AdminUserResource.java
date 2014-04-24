e512ea79bb8c9a94fec16d41e5f078a45920ee40
vbroussard <vbroussard@users.noreply.github.com>
2014-04-24 06:57:28 +0000

    RESADMIN-1 : Creation of module resource admin user
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/module-resource-adminuser@66011 bab10101-e421-0410-a517-8ce0973de3ef

---
+++ src/java/fr/paris/lutece/plugins/resource/modules/adminuser/business/AdminUserResource.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2002-2014, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.resource.modules.adminuser.business;
+
+import fr.paris.lutece.plugins.resource.business.IResource;
+import fr.paris.lutece.portal.business.user.AdminUser;
+
+
+/**
+ * Admin user resource
+ */
+public class AdminUserResource extends AdminUser implements IResource
+{
+
+    private static final long serialVersionUID = -4011512795595150982L;
+    private static final String CONSTANT_SPACE = " ";
+
+    /**
+     * Default constructor
+     */
+    public AdminUserResource( )
+    {
+        // Default constructor
+    }
+
+    /**
+     * Creates a new admin user resource from an admin user
+     * @param user The user
+     */
+    public AdminUserResource( AdminUser user )
+    {
+        setUserId( user.getUserId( ) );
+        setAccessCode( user.getAccessCode( ) );
+        setLastName( user.getLastName( ) );
+        setFirstName( user.getFirstName( ) );
+        setEmail( user.getEmail( ) );
+        setStatus( user.getStatus( ) );
+        setUserLevel( user.getUserLevel( ) );
+        setPasswordReset( user.isPasswordReset( ) );
+        setAccessibilityMode( user.getAccessibilityMode( ) );
+        setPasswordMaxValidDate( user.getPasswordMaxValidDate( ) );
+        setAccountMaxValidDate( user.getAccountMaxValidDate( ) );
+        setDateLastLogin( user.getDateLastLogin( ) );
+        setRights( user.getRights( ) );
+        setRoles( user.getRoles( ) );
+        setAuthenticationService( user.getAuthenticationService( ) );
+        setAuthenticationType( user.getAuthenticationType( ) );
+        setLocale( user.getLocale( ) );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getIdResource( )
+    {
+        return Integer.toString( getUserId( ) );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getResourceType( )
+    {
+        return AdminUser.RESOURCE_TYPE;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getResourceName( )
+    {
+        return getFirstName( ) + CONSTANT_SPACE + getLastName( );
+    }
+
+}
---
+++ src/java/fr/paris/lutece/plugins/resource/modules/adminuser/service/provider/AdminUserResourceProvider.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2002-2014, Mairie de Paris
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ *  1. Redistributions of source code must retain the above copyright notice
+ *     and the following disclaimer.
+ *
+ *  2. Redistributions in binary form must reproduce the above copyright notice
+ *     and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ *
+ *  3. Neither the name of 'Mairie de Paris' nor 'Lutece' nor the names of its
+ *     contributors may be used to endorse or promote products derived from
+ *     this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ * License 1.0
+ */
+package fr.paris.lutece.plugins.resource.modules.adminuser.service.provider;
+
+import fr.paris.lutece.plugins.resource.business.IResource;
+import fr.paris.lutece.plugins.resource.business.IResourceType;
+import fr.paris.lutece.plugins.resource.business.ResourceTypeDefaultImplementation;
+import fr.paris.lutece.plugins.resource.modules.adminuser.business.AdminUserResource;
+import fr.paris.lutece.plugins.resource.service.ResourceCacheService;
+import fr.paris.lutece.plugins.resource.service.provider.IResourceProvider;
+import fr.paris.lutece.portal.business.user.AdminUser;
+import fr.paris.lutece.portal.business.user.AdminUserHome;
+import fr.paris.lutece.portal.service.i18n.I18nService;
+
+import org.apache.commons.lang.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Locale;
+
+
+/**
+ * Resource provider for admin users
+ */
+public class AdminUserResourceProvider implements IResourceProvider
+{
+
+    private static final String MESSAGE_ADMIN_USER_RESOURCE_TYPE_DESCRIPTION = "module.resource.adminuser.labelAdminUserResourceType";
+
+    private List<IResourceType> _listResourceTypes;
+
+    /**
+     * Default constructor
+     */
+    public AdminUserResourceProvider( )
+    {
+        _listResourceTypes = new ArrayList<IResourceType>( 1 );
+        _listResourceTypes.add( new ResourceTypeDefaultImplementation( AdminUser.RESOURCE_TYPE, I18nService
+                .getLocalizedString( MESSAGE_ADMIN_USER_RESOURCE_TYPE_DESCRIPTION, Locale.getDefault( ) ) ) );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<IResourceType> getResourceTypeList( )
+    {
+        return _listResourceTypes;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isResourceTypeManaged( String strResourceTypeName )
+    {
+        return AdminUser.RESOURCE_TYPE.equals( strResourceTypeName );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public IResource getResource( String strIdResource, String strResourceTypeName )
+    {
+        String strCacheKey = ResourceCacheService.getResourceCacheKey( strIdResource, strResourceTypeName );
+        AdminUserResource user = (AdminUserResource) ResourceCacheService.getInstance( ).getFromCache( strCacheKey );
+        if ( user == null )
+        {
+            if ( StringUtils.isNotEmpty( strIdResource ) && StringUtils.isNumeric( strIdResource ) )
+            {
+                int nIdAdminUser = Integer.parseInt( strIdResource );
+                user = new AdminUserResource( AdminUserHome.findByPrimaryKey( nIdAdminUser ) );
+                ResourceCacheService.getInstance( ).putInCache( strCacheKey, user );
+            }
+        }
+        return user;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<IResource> getListResources( )
+    {
+        Collection<AdminUser> listAdminUser = AdminUserHome.findUserList( );
+        List<IResource> listResource = new ArrayList<IResource>( listAdminUser.size( ) );
+        for ( AdminUser adminUser : listAdminUser )
+        {
+            listResource.add( new AdminUserResource( adminUser ) );
+        }
+
+        return listResource;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public List<IResource> getListResources( String strResourceTypeName )
+    {
+        if ( AdminUser.RESOURCE_TYPE.equals( strResourceTypeName ) )
+        {
+            return getListResources( );
+        }
+        return new ArrayList<IResource>( );
+    }
+}
